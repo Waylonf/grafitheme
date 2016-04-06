08054cd58e6123ce219c6846f94089605b6fecfe
Waylon <waylon@grafika.co.za>
2016-04-06 15:24:37 +0200

    Added sandbox.php for smooth function and theme testing

---
+++ inc/grafipress-sandbox.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit;
+/**
+ * WARNING: This file is part of the core {THEME-NAME} framework. DO NOT edit this
+ * file under any circumstances. Please do all modifications
+ * in the form of a child theme.
+ *
+ * This file contains functions that may be included into the theme package. They are
+ * placed withing this file for testing and separated from the production files 
+ * for debugging. This file is bypassed by default if the DEBUG constant is set to false.
+ *
+ * @package     WordPress
+ * @subpackage  {THEME-NAME}
+ * @since       {THEME-NAME} {THEME-VERSION}
+ */
